19a8817206ca8a9c70e335df3c852569521c89d4
宋曦 <wanghan@iching.ac.cn>
2026-03-25 11:07:44 +0800

    deploy site: script.js

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+/* script.js - General functionality for Science & Humanities Review */
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Mobile Navigation logic or other interactive features can be added here
+    console.log('Science & Humanities Review - Website Loaded');
+});
